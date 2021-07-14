3c3172795d3228e49225b4c17f0e5dc73949b99f
yuvalnir <yuvalnir11@gmail.com>
2021-07-14 19:10:41 +0300

    added photos + previews to add recipe page

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -5,7 +5,11 @@ class AddRecipe extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { ingredients: [''] };
+        this.state = { 
+            ingredients: [''],
+            images: [],
+            preview: []
+        };
     }
 
     handleAddRecipe = (e) => {
@@ -15,7 +19,7 @@ class AddRecipe extends Component {
         const data = new FormData(e.target);
         console.log("{title: " + data.get('recipe-title') + " ingredients: " + data.get('recipe-ingredients') + " instructions: " + data.get('recipe-instructions') + "}"); //test print
         
-        //creating the ingredients array that will be passed to the db
+        //creating the ingredients array to be passed to the db
         const ingNames = data.getAll("recipe-ingredients-name");
         const ingAmount = data.getAll("recipe-ingredients-amount");
         const ingAmountType = data.getAll("recipe-ingredients-amount-type");
@@ -44,7 +48,7 @@ class AddRecipe extends Component {
             .catch(err => { console.log(err) })
     }
 
-    handleDelete = index => e => {
+    deleteIngredient = index => e => {
         e.preventDefault()
         const ingredients = this.state.ingredients;
         ingredients.splice(index, 1);
@@ -59,52 +63,95 @@ class AddRecipe extends Component {
         this.setState({ ingredients: ingredients })
     }
 
+    handleFileSelected = e => {
+        e.preventDefault()
+        const files = e.target.files;
+            if (files) {
+              this.setState({images: files});
+              for (let i = 0; i < e.target.files.length; i++) {
+                  if(e.target.files[i].type.substr(0, 5) === "image") {
+                    const reader = new FileReader();
+                    reader.onloadend = () => {
+                      this.setState({ preview: [...this.state.preview, reader.result]});
+                    };
+                    reader.readAsDataURL(e.target.files[i]);
+                  }
+              }
+            } else {
+                this.setState({images: []});
+            }
+    }
+
+    removePhoto = index => e => {
+        e.preventDefault()
+        const previewImages = this.state.preview;
+        previewImages.splice(index, 1);
+        
+        this.setState({ preview: previewImages });
+    }
+
     render() {
 
         return (
             <div className="add-recipe-page">
-                <div className="add-page-title"> <h1>Add Recipe</h1> </div>
+                
 
                 <form className="add-recipe-page-form" onSubmit={this.handleAddRecipe.bind(this)}>
+                    <div className="add-page-header"> <h1>Add Recipe</h1> </div>
 
-                    <div className="form-group">
+                    <div className="title-form-group">
                         <div className="add-recipe-title"> Title </div>
-                        <input type="text" name="recipe-title" className="form-control" placeholder="Place title here" required />
+                        <input type="text" name="recipe-title" className="title-input form-control" placeholder="Place title here" required />
                     </div>
 
-                    <div className="form-group ingredient-form-group">
+                    <div className="ingredient-form-group">
                         <div className="add-recipe-title"> Ingredients </div>
                         <div className="ingredient-box">
                             {this.state.ingredients.map((ingredient, index) => (
-                                <div className="input-box" key={index}>
+                                <div className="ingredient-input-box" key={index}>
                                     <input type="text" name="recipe-ingredients-name" className="form-control" placeholder="Name" required />
                                     <input type="text" name="recipe-ingredients-amount" className="form-control amount" placeholder="Amount" required />
                                     <input type="text" name="recipe-ingredients-amount-type" className="form-control measure" placeholder="Unit type" />
-                                    <button className="btn btn-light btn-block" onClick={this.handleDelete(index).bind(this)}>X</button>
+                                    <button className="btn btn-light btn-block" onClick={this.deleteIngredient(index).bind(this)}>X</button>
                                 </div>
                             ))}
                         </div>
-                        
+
+                        <button className="ingredient-btn btn-width btn btn-light btn-block" onClick={this.addIngredient.bind(this)}>Add Ingredient</button>
                     </div>
 
-                    <div className="form-group add-ingredient-btn">
-                            <button className="btn btn-light btn-block" onClick={this.addIngredient.bind(this)}>Add Ingredient</button>
-                        </div>
-
-
-                    <div className="form-group">
+                    <div className="instructions-form-group">
                         <div className="add-recipe-title"> Instructions </div>
                         <textarea name="recipe-instructions" rows="8" className="add-recipe-instructions-input form-control " placeholder="Place instructions here" required />
                     </div>
 
 
-                    <div className="form-group">
-                        <div className="add-recipe-title"> Add photos Coming Soon... </div>
-                        {/* <button type="button" className="btn btn-light btn-block">+</button> */}
+                    <div className="photos-form-group">
+                        <input 
+                            type="file"
+                            accept="image/*"
+                            multiple="multiple"
+                            style={{display: 'none'}}
+                            onChange={this.handleFileSelected}
+                            ref={fileInput => this.fileInput = fileInput}
+                        />
+                        <button type="button" onClick={() => this.fileInput.click()} className="btn-width btn btn-light btn-block">Choose photos</button>
+                        <div className="photos-container">
+                            {this.state.preview ? 
+                                this.state.preview.map( (image, index) => (
+                                    <div>
+                                        <img src={image} key={index} />
+                                        <button className="close-btn" onClick={this.removePhoto(index)}>X</button>
+                                    </div>
+                                ))
+                                : ''
+                            }
+                            
+                        </div>
                     </div>
 
 
-                    <button type="submit" className="btn btn-light btn-block">Add Recipe</button>
+                    <button type="submit" className="btn-width btn-top-margin add-page-footer btn btn-light btn-block">Add Recipe</button>
 
                     {/* <div className="add-recipe-title"> Add video (maybe in the future) </div> */}
                 </form>
