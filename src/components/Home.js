bd148b7a9aa372c9e05de7b5058c40c9401eab63
yuvalnir <yuvalnir11@gmail.com>
2021-07-22 14:29:42 +0300

    added recipe image display as cover and passing as props to recipe card

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,6 @@ import React, { Component } from "react";
 import Button from 'react-bootstrap/Button';
 
 import RecipeCard from "./RecipeCard";
-import babka from "../temp-images/babka.jpg";
-import SmoresPie from "../temp-images/Smores Pie.jpg";
-import lemonPie from "../temp-images/lemonpei.jpg";
 
 class Home extends Component {
 
@@ -12,6 +9,7 @@ class Home extends Component {
     super()
     this.state = {
       recipes: [],
+      imagesArr: [],
     }
   }
 
@@ -20,14 +18,16 @@ class Home extends Component {
   }
 
   fetchRecipes = () => {
-    fetch(`http://localhost:8081/recipeapi/userrecipes/${localStorage.getItem('userEmail')}`, {
+    fetch(`http://localhost:8081/recipeapi/userrecipes/${localStorage.getItem('userId')}`, {
       method: 'GET',
       headers: { 'Content-Type': 'application/json' },
     })
       .then(response => {
         if (response.status === 200) {
           response.json().then(jsonObj => {
-            this.setState({ recipes: jsonObj.data });
+            this.setState({ recipes: jsonObj.data.recipes });
+            this.setState({ imagesArr: jsonObj.data.imagesArr });
+            console.log(jsonObj.data);
           });
         }
       })
@@ -50,20 +50,20 @@ class Home extends Component {
         <div className="button-div"> <Button variant="light" onClick={() => this.clickLeft('myFavoriteRecipes')}>L</Button> </div>
         <div className="card-slide-container" id='myFavoriteRecipes'>
           {this.state.recipes.map((recipe, index) => {
-            return <RecipeCard recipe={recipe} key={recipe._id} />
+            return <RecipeCard recipe={recipe} images={this.state.imagesArr[index]} key={recipe._id} />
           })}
         </div>
         <div className="button-div"> <Button variant="light" onClick={() => this.clickRight('myFavoriteRecipes')}>R</Button> </div>
         {/* </div> */}
-        {/* <div className="new-recipies"> */}
+        {/* <div className="new-recipes"> */}
         <div className="title-div"> <h6>New recipes</h6> </div>
-        <div className="button-div"> <Button variant="light" onClick={() => this.clickLeft('new-recipies')}>L</Button> </div>
-        <div className="card-slide-container" id='new-recipies'>
+        <div className="button-div"> <Button variant="light" onClick={() => this.clickLeft('new-recipes')}>L</Button> </div>
+        <div className="card-slide-container" id='new-recipes'>
           {this.state.recipes.map((recipe, index) => {
-            return <RecipeCard recipe={recipe} key={recipe._id} />
+            return <RecipeCard recipe={recipe} image={this.state.imagesArr[index]} key={recipe._id} />
           })}
         </div>
-        <div className="button-div"> <Button variant="light" onClick={() => this.clickRight('new-recipies')}>R</Button> </div>
+        <div className="button-div"> <Button variant="light" onClick={() => this.clickRight('new-recipes')}>R</Button> </div>
       </div>
       // </div>
     );
