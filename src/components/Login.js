d29573d1fad6823e759f9a61176eefe0b381bff1
yuvalnir <yuvalnir11@gmail.com>
2021-07-22 14:25:35 +0300

    .

--- src/components/Login.js
+++ src/components/Login.js
@@ -50,17 +50,17 @@ class Login extends Component {
                     <input type="password" name="password" className="form-control" placeholder="Enter password" required/>
                 </div>
 
-                <div className="form-group">
+                {/* <div className="form-group">
                     <div className="custom-control custom-checkbox">
                         <input type="checkbox" className="custom-control-input" id="customCheck1" />
                         <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
                     </div>
-                </div>
+                </div> */}
 
                 <button type="submit" className="btn btn-primary btn-block">Submit</button>
-                <p className="forgot-password text-right">
+                {/* <p className="forgot-password text-right">
                     Forgot <a href="#">password?</a>
-                </p>
+                </p> */}
             </form>
         );
     }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -65,9 +65,9 @@ class SignUp extends Component {
                 </div>
 
                 <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
-                <p className="forgot-password text-right">
+                {/* <p className="forgot-password text-right">
                     Already registered <a href="#">sign in?</a>
-                </p>
+                </p> */}
             </form>
         );
     }
